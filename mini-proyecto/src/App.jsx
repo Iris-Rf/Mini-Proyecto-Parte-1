8b948848bf91318dee32645bca7ec5baa5c5d726
Iris-Rf <irys@hotmail.com>
2022-10-25 15:05:03 +0200

    mini-proyect

---
+++ mini-proyecto/src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  const [characterList, setCharacterList] = React.useState([]);
+
+  React.useEffect(() => {
+    (async () => {
+      let data = await fetch(`https://rickandmortyapi.com/api/character/`).then(
+        (res) => res.json()
+      );
+
+      setCharacterList(data.results);
+    })();
+  }, []);
+
+  return (
+    <>
+      {characterList.map((character) => (
+        <div key={character.id}>
+
+          <h2>name: {character.name}</h2>
+   
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default App;
